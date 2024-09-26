05402ef13b0bd639a21bdbbb19d66a449ab084de
bibeast <160469535+bibeast@users.noreply.github.com>
2024-09-26 12:21:57 -0500

    deploy

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bibi-fitness-api.onrender.com",
+  baseURL: "https://fitness-backend-je4w.onrender.com",
   // baseURL: "https://localhost:3001",
   withCredentials: true,
 });
 
 export default api;
+
+// baseURL: "https://bibi-fitness-api.onrender.com",
