4c2ede911fba1b7c3163cc1398e4cdb57d1b9eb2
bibeast <160469535+bibeast@users.noreply.github.com>
2024-09-27 16:25:55 -0500

    changes to vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: "/index.html", // this tells Vite where to look for the main HTML file
+    },
+  },
+  server: {
+    hmr: true, // optional: Hot module reload
+    historyApiFallback: true, // <-- this is important for React Router
+  },
 });
